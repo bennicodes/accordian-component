8aba5f1382dad76ee150339d9132d196818084f2
Benedict Notoane <benni@Benedicts-MacBook-Pro.local>
2024-11-20 13:19:53 +0100

    added functionality

---
+++ src/js/accordion.js
@@ -0,0 +1,31 @@
+const titleContainers = document.querySelectorAll(
+  ".accordion__title-container"
+);
+const descriptionContainers = document.querySelectorAll(
+  ".accordion__description-container"
+);
+
+const accordionToggleLogos = document.querySelectorAll(
+  ".accordion__toggle-logo"
+);
+
+// titleContainers.forEach((title, index) => {
+//   title.addEventListener("click", () => {
+//     descriptionContainers.forEach((description, index) => {
+//       descriptionContainers[index].classList.toggle(
+//         "accordion__description-container--active"
+//       );
+//     });
+//     accordionToggleLogos[index].classList.toggle("rotate-arrow");
+//   });
+// });
+
+titleContainers.forEach((title, index) => {
+  title.addEventListener("click", () => {
+    descriptionContainers[index].classList.toggle(
+      "accordion__description-container--active"
+    );
+
+    accordionToggleLogos[index].classList.toggle("rotate-arrow");
+  });
+});
